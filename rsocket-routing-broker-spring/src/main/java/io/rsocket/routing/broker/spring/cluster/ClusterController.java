6c114475d38c3f5729eff330e977c07f6ade9753
Spencer Gibb <spencer@gibb.us>
2020-02-11 18:04:02 -0500

    Adds skeleton for ClusterController

---
+++ rsocket-routing-broker-spring/src/main/java/io/rsocket/routing/broker/spring/cluster/ClusterController.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.rsocket.routing.broker.spring.cluster;
+
+import reactor.core.publisher.Mono;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ClusterController {
+
+	public ClusterController() {
+		System.out.println();
+	}
+
+	@MessageMapping("hello")
+	public Mono<String> hello(String name) {
+		return Mono.just("Hello " + name);
+	}
+
+}
